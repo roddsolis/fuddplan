fd49497c192d50b3f2463938873ce44d60986c61
rodrigo solis <rodrigo.solis.g@gmail.com>
2024-09-29 00:00:47 -0300

    primer commit del proyecto fuddplan

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,10 +19,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+    <html lang="es">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>{children}</body>
     </html>
   );
 }

48dea1d104c5a87bd7b907d68cbe5f7f0fba6f91
rodrigo solis <rodrigo.solis.g@gmail.com>
2024-09-29 22:40:05 -0300

    agregue algunas carpetas para la estructura del proyecto

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+/* import styles from "./css/page.module.css"; */ /* esta linea aplica el css de manera modular  */
 
 export default function Home() {
   return (

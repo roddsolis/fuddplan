5e5c6fd269af0f81c70f33f2d48bf76f58e554ed
rodrigo solis <rodrigo.solis.g@gmail.com>
2024-10-01 02:00:20 -0300

    avances en las rutas del proyecto, se agrega paso a paso

--- src/app/calculadora/page.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Calculadora = () => {
-  return <div>esta es la pagina de la calculadora</div>;
-};
-
-export default Calculadora;
---
+++ src/app/steps/[step]/page.js
@@ -0,0 +1,7 @@
+"use client";
+
+const Step1Content = () => {
+  return <div>paso 1</div>;
+};
+
+export default Step1Content;

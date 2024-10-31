82fd8a997ed40a89c9ccd3db0b9eddbc1e61d3e5
rodrigo solis <rodrigo.solis.g@gmail.com>
2024-10-31 14:42:01 -0300

    se agrego una clase para definir los datos que se le solicitan al usuario y pued almacenar la informacion

--- src/app.py
+++ src/app.py
@@ -1,28 +1,44 @@
 """ calculadora de IMC """
 
-def calcular_imc():
-    """ esta funcion obtiene el dato de peso y altura del usuario """
-    peso_del_usuario = float(input('¿Cuál es tu peso actual en kg? '))
-    estatura_del_usuario = float(input('¿Cuál es tu estatura en metros? (por ejemplo, 1.75) '))
-    imc_del_usuario = peso_del_usuario / (estatura_del_usuario ** 2)
-    return round(imc_del_usuario,1)
+class User:
+    def __init__(self):
+        self.peso_del_usuario = 0
+        self.estatura_del_usuario = 0
+
+    def datos_de_usuario (self):
+        """ esta funcion captura los datos del usuario """
+        self.peso_del_usuario = float(input('¿Cuál es tu peso actual en kg? '))
+        self.estatura_del_usuario = float(input('¿Cuál es tu estatura en metros? (ejemplo 1.75) '))
+
+    def calcular_imc(self):
+        """ esta funcion obtiene el dato de peso y altura del usuario """
+        imc_del_usuario = self.peso_del_usuario / (self.estatura_del_usuario ** 2)
+        return round(imc_del_usuario,1)
+
+    def definir_rango_salud (self, imc):
+        """ esta funcion clasifica al usuario en un rango de salud"""
+        if imc < 18.5:
+            return 'bajo peso'
+        elif imc > 18.5 and imc < 24.9:
+            return 'peso normal'
+        elif imc > 25.0 and imc < 29.9:
+            return 'sobrepeso'
+        elif imc > 30.0 and imc < 34.9:
+            return 'obesidad grado 1'
+        elif imc > 35.0 and imc < 39.9:
+            return 'obecidad grado 2'
+        elif imc > 40:
+            return 'obecidad grado 3'
+        else:
+            return 'ocurrio un error intenta nuevamente'
 
 
+usuario = User()
 
-resultado_imc = calcular_imc()
+usuario.datos_de_usuario()
 
-""" aca se define el rango de salud del usuario """
-if resultado_imc < 18.5:
-    print('bajo peso')
-elif resultado_imc > 18.5 and resultado_imc < 24.9:
-    print('peso normal')
-elif resultado_imc > 25.0 and resultado_imc < 29.9:
-    print('sobrepeso')
-elif resultado_imc > 30.0 and resultado_imc < 34.9:
-    print('obesidad grado 1')
-elif resultado_imc > 35.0 and resultado_imc < 39.9:
-    print('obecidad grado 2')
-elif resultado_imc > 40:
-    print('obecidad grado 3')
-else:
-    print('ocurrio un error intenta nuevamente')
+resultado_imc = usuario.calcular_imc()
+
+
+print(f'El resultado de tu IMC es de {resultado_imc}')
+print(usuario.definir_rango_salud(resultado_imc))

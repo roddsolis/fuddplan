4b3a589b63ca01bcffed7615ed7e7bd242227c6a
rodrigo solis <rodrigo.solis.g@gmail.com>
2024-11-07 22:54:28 -0300

    agregando nuevos campos de datos

--- src/app/step-1/page.js
+++ src/app/step-1/page.js
@@ -5,10 +5,13 @@ const DatosPersonales = () => {
   const [nombre, setNombre] = useState("");
   const [apellidos, setApellidos] = useState("");
   const [email, setEmail] = useState("");
+  const [edad, setEdad] = useState("");
+  const [mujer, setMujer] = useState("");
+  const [hombre, setHombre] = useState("");
 
   const enviarDatos = async (e) => {
-    e.preventDefault(); // Evitar que la página se recargue al enviar el formulario
-    const datos = { nombre, apellidos, email };
+    e.preventDefault();
+    const datos = { nombre, apellidos, email, edad, mujer, hombre };
 
     try {
       const response = await fetch("http://localhost:5000/user_data", {
@@ -47,6 +50,19 @@ const DatosPersonales = () => {
           Email
           <input type="email" placeholder="Ingresar email" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
         </label>
+        <label htmlFor="edad">
+          Edad
+          <input type="number" placeholder="Ingresar tu edad" name="edad" id="edad" value={edad} onChange={(e) => setEdad(e.target.value)} />
+        </label>
+        <label htmlFor="mujer">
+          Mujer
+          <input type="radio" name="mujer" id="mujer" value={mujer} onChange={(e) => setMujer(e.target.value)} />
+        </label>
+        <label htmlFor="hombre">
+          Hombre
+          <input type="radio" name="hombre" id="hombre" value={hombre} onChange={(e) => setHombre(e.target.value)} />
+        </label>
+
         <button type="submit">Enviar</button>
       </form>
     </>

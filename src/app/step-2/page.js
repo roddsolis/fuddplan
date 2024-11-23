9e6f5ba2810b42c0df8384a29320678a0fd669ff
rodrigo solis <rodrigo.solis.g@gmail.com>
2024-11-23 20:24:47 -0300

    trabajando en la integracion de estilos de forma modular

--- src/app/step-2/page.js
+++ src/app/step-2/page.js
@@ -1,12 +1,28 @@
 "use client";
 import React from "react";
 import useUserStore from "../useUserStore";
+import styles from "/src/css/DatosDeSalud.module.css";
 
 const DatosDeSalud = () => {
   const nombre = useUserStore((state) => state.nombre);
   return (
     <>
-      <h1>{nombre ? `${nombre}` : ""}, para comenzar con tu evaluacion ingresa los datos necesarios en cada paso</h1>
+      <h1>{nombre}cual es tu nivel de actividad física</h1>
+      <h3>elige una opción</h3>
+      <div className={styles.optionContainer}>
+        <div className="cardOption">
+          <input type="radio" />
+          <h6>Sedentario</h6>
+        </div>
+        <div className="cardOption">
+          <input type="radio" />
+          <h6>Activo</h6>
+        </div>
+        <div className="cardOption">
+          <input type="radio" />
+          <h6>Muy activo</h6>
+        </div>
+      </div>
     </>
   );
 };

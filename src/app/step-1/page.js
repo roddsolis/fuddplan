07e2567cc4a686e9c3421e067208a55985075628
rodrigo solis <rodrigo.solis.g@gmail.com>
2024-11-24 20:45:22 -0300

    se agregaron estilos para cada paso

--- src/app/step-1/page.js
+++ src/app/step-1/page.js
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import useUserStore from "../useUserStore";
+import styles from "@/css/datosPersonales.module.css";
 
 const DatosPersonales = () => {
   const [nombre, setNombre] = useState("");
@@ -48,38 +49,40 @@ const DatosPersonales = () => {
   };
 
   return (
-    <>
-      <h1>¡Bienvenido a Fuddplan!</h1>
-      <p>Ingresa tus datos para poder obtener un perfil de salud y te ayudaremos a mejorar tu alimentación en base a tu objetivo principal.</p>
-      <form onSubmit={enviarDatos}>
-        <label htmlFor="nombre">
+    <div className={styles.mainContainer}>
+      <div className={styles.textContainer}>
+        <h1>¡Bienvenido a Fuddplan!</h1>
+        <p>Ingresa tus datos para poder obtener un perfil de salud y te ayudaremos a mejorar tu alimentación en base a tu objetivo principal.</p>
+      </div>
+      <form onSubmit={enviarDatos} className={styles.formContainer}>
+        <label htmlFor="nombre" className={styles.textFieldContainer}>
           Nombre
           <input type="text" placeholder="Ingresar nombre" name="nombre" id="nombre" value={nombre} onChange={(e) => setNombre(e.target.value)} />
         </label>
-        <label htmlFor="apellidos">
+        <label htmlFor="apellidos" className={styles.textFieldContainer}>
           Apellidos
           <input type="text" placeholder="Ingresar apellidos" name="apellidos" id="apellidos" value={apellidos} onChange={(e) => setApellidos(e.target.value)} />
         </label>
-        <label htmlFor="email">
+        <label htmlFor="email" className={styles.textFieldContainer}>
           Email
           <input type="email" placeholder="Ingresar email" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
         </label>
-        <label htmlFor="edad">
+        <label htmlFor="edad" className={styles.textFieldContainer}>
           Edad
           <input type="number" placeholder="Ingresar tu edad" name="edad" id="edad" value={edad} onChange={(e) => setEdad(e.target.value)} />
         </label>
-        <label htmlFor="mujer">
+        <label htmlFor="mujer" className={styles.textFieldContainer}>
           Mujer
           <input type="radio" name="sexo" id="mujer" value="mujer" onChange={handleOptionChange} checked={sexo === "mujer"} />
         </label>
-        <label htmlFor="hombre">
+        <label htmlFor="hombre" className={styles.textFieldContainer}>
           Hombre
           <input type="radio" name="sexo" id="hombre" value="hombre" onChange={handleOptionChange} checked={sexo === "hombre"} />
         </label>
 
         <button type="submit">Enviar</button>
       </form>
-    </>
+    </div>
   );
 };
 
